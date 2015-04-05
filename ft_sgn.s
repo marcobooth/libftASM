5ab0fbecfa136d50aec801616d5bb47b2debf83c
Marco BOOTH <mbooth@e1r5p15.42.fr>
2015-04-05 19:36:06 +0200

    nearly

--- ft_sgn.s
+++ ft_sgn.s
@@ -6,7 +6,7 @@
 ;    By: mbooth <mbooth@student.42.fr>              +#+  +:+       +#+         ;
 ;                                                 +#+#+#+#+#+   +#+            ;
 ;    Created: 2015/04/05 19:16:07 by mbooth            #+#    #+#              ;
-;    Updated: 2015/04/05 19:18:25 by mbooth           ###   ########.fr        ;
+;    Updated: 2015/04/05 19:29:09 by mbooth           ###   ########.fr        ;
 ;                                                                              ;
 ;******************************************************************************;
 
@@ -15,4 +15,19 @@ section .text:
 
 _ft_sgn:
 	cmp rdi, 0
+	je its_zero
+	jg more_than_zero
+	jl less_than_zero
+
+its_zero:	
+	mov rax, 0
+	ret
+
+more_than_zero:	
+	mov rax, 1
+	ret
+
+less_than_zero:	
+	mov rax, -1
+	ret
 	
